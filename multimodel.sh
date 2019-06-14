29c388c0c4a071dced8c3314ba5f5f122298e189
Mohamed Elansary (DS) <Elansary@hpcc2.cm.cluster>
2019-06-14 07:33:32 -0500

    Fri Jun 14 07:33:32 CDT 2019

--- multimodel.sh
+++ multimodel.sh
@@ -5,10 +5,14 @@ for f in {cfsv2,echam4p5}/Sites/*/3_forecast/CV/CV.*;do awk 'NR>6{split(FILENAME
 
 for x in 08080500 08085500 08151500 08194500;do
   for l in 1 2 3 4;do
-    awk 'FNR==1{++f}f==1{obs[$1" "$2]=$3;clim[$2]+=$3;xclim[$2]++}f==2{m1[$1" "$2]=$3}f==3{print $1,$2,obs[$1" "$2],clim[$2]/xclim[$2],m1[$1" "$2],$3}' \
-    Sites/data/$x.obs Sites/data/cfsv2.$x.$l.data Sites/data/echam4p5.$x.$l.data > Sites/data/$x.$l.all;
+    awk 'FNR==1{++f}f==1{obs[$1" "$2]=$3;clim[$2]+=$3;xclim[$2]++}f==2{m1[$1" "$2]=$3}f==3{print $1,$2,'"$l"',obs[$1" "$2],clim[$2]/xclim[$2],m1[$1" "$2],$3}' \
+    Sites/data/$x.obs Sites/data/cfsv2.$x.$l.data Sites/data/echam4p5.$x.$l.data > Sites/data/$x.$l.data;
   done;
+  cat Sites/data/$x.?.data > Sites/data/$x.all;
 done;
+ll Sites/data/*.all
+
+
 
 #for l in 1 2 3 4;do f={cfsv2,echam4p5}/Sites/*/3_forecast/CV/CV.$l.txt;echo $f Sites/data/$(sed 's/\/Sites\//_/g; s/\/.*$//g' <<<$f).data;done
 #68.6624.190612.133101 Elansary@hpcc2 ~/phd/paper_1 $
